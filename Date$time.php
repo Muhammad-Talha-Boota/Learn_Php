dfc70459d9d94b8994e894f915db36172ebbca1c
Asad <asadb4998@gmail.com>
2022-04-22 14:54:11 +0500

    Some new Code In Date$time Folder. There is Some Function For Date and Time

--- Date$time.php
+++ Date$time.php
@@ -96,7 +96,7 @@ echo "Current Meridian is : " . date('A') . "<br>"; //For maredian in Capital Ca
 
 // Full Time 
 
-date_default_timezone_set("Asia/Karachi");
+date_default_timezone_set("Asia/Karachi"); // For Set Time Zone
 echo "<h1>Time is " . date('h:i:s A e') . "</h1><br>";
 echo "<h1>Date & Time is " . date('dS/M/Y h:i:s A e') . "</h1><br>"; // e parameter for Time Zone
 */ 
@@ -114,6 +114,286 @@ echo date("d/M/Y h:i:s A" , mktime(0,0,0,10,15,2003)) ."<br>";
 
 echo date("d/M/Y h:i:s A" , gmmktime(0,0,0,10,15,2003)) ."<br>";
 */
+ 
+
+
+                  ####### date_create() & date_format() Function #########
+
+/*                  
+//// date_create() Function is use for Create Futcher date or Past Date. This Function Have Two parametrs First is Date&Time Second is timezone. And This Function Return a Object.
+
+$date = date_create("2050-12-23 12:23:00",timezone_open('Asia/Karachi')); //we have time zone in this formate with timezone_open.
+
+//// date_format() function is use for Show the date_create Function.This Function Have two parametrs First is Object That we Have in date_create Function and Second is Formate. 
+
+echo date_format($date,"F/d/Y h:i:s");
+*/
+
+
+                  ####### cheakdate() & date_diff() Function #########
+
+/*
+//// cheakdate() Function is use for Cheak the dates This Function have three parameter First is Month second is Date and third is Year. This Function Return 1 in case of true and return 0 in the case of false. In this Function we Check Past and Futher Dates.
+
+echo checkdate(2,29,2004) . "<br>";
+echo checkdate(3,25,2021) . "<br>";
+echo checkdate(7,12,2030) . "<br>";
+echo checkdate(4,15,2001) . "<br>";
+
+
+//// date_diff() Function is use for taken Diffrenace Between two dates. This Function have three dates First is date1 second is date2 and third is absolute(this have to parameters True or False false is Default).This Function Return Differncee of Dates Between two dates.
+
+$date1 = date_create("2001-04-06 2:12:00");
+$date2 = date_create("2022-04-19 2:12:00");
+
+$diff = date_diff($date1, $date2);
+
+echo $diff->days . "Days";
+echo "<pre>";
+print_r($diff);
+
+// For Negitive Value
+
+$date1 = date_create("2013-12-15");
+$date2 = date_create("2013-03-12");
+
+$diff = date_diff($date1, $date2);
+
+echo $diff->format("%R%a days"); // R is use for negitive value and % sign is Return Percentage this sign is use for days months years etc.
+echo "<pre>";
+print_r($diff);
+
+// For Positive Value
+
+$date1 = date_create("2013-03-15");
+$date2 = date_create("2013-12-12");
+
+$diff = date_diff($date1, $date2);
+
+echo $diff->format("%R%a days"); // r is use for negitive sign not for positive sign R is use for negitive value and % sign is Return Percentage this sign is use for days months years etc.
+echo "<pre>";
+print_r($diff);
+*/
+
+
+
+         
+              ####### date_add() , date_sub() & date_modify() Function #########
+
+/*
+//// date_add() Function is use for add some Days in the Existing Date. This Function Have Two parameters First is Object that is Return From Date_create() And Sencod is Intervel.
+
+$date = Date_create("2016-05-15");
+
+date_add($date,date_interval_create_from_date_string("30 days")); //2nd parameter use for change string into intervel
+
+echo date_format($date, 'd-M-Y') . "<br>"; 
+
+//// date_sub() Function is use for Subtraction some Days in the Existing Date. This Function Have Two parameters First is Object that is Return From Date_create() And Sencod is Intervel.
+
+$date = Date_create("2016-05-15");
+
+date_sub($date,date_interval_create_from_date_string("10 days")); //2nd parameter use for change string into intervel
+
+echo date_format($date, 'd-M-Y') . "<br>"; 
+
+//// date_modify() Function is use For Both Sub and Add. In this Function on the second parametr we have only string Without any Function.
+
+$date = Date_create("2016-05-15");
+
+date_modify($date,"10 days"); 
+
+echo date_format($date, 'd-M-Y') . "<br>"; 
+ 
+// For subtraction of the date.
+
+$date = Date_create("2016-05-15");
+
+date_modify($date,"-30 days"); 
+
+echo date_format($date, 'd-M-Y') . "<br>"; 
+*/
+
+
+
+              ####### getdate() , localtime() & gettimeofday() Function #########
+ 
+/*           
+//// getdate() Function is return a accosiative array of the current day & time. This Function is also use for Past Date.
+
+$date = getdate();
+
+echo $date['mday'] . "-" . $date['month'] . "-" . $date['year'];
+
+echo "<pre>";
+print_r(getdate());
+echo "</pre>";
+
+// For old Date
+
+$old_date = mktime(01,15,30,4,6,2001);
+
+$date = getdate($old_date);
+
+echo $date['mday'] . "-" . $date['month'] . "-" . $date['year'];
+
+echo "<pre>";
+print_r(getdate($old_date));
+echo "</pre>";
+
+
+//// gettimeifday() Function is use for Have Current Time if the Day in the Array Form.
+
+$time = gettimeofday();
+
+echo $time['usec'];
+
+echo "<pre>";
+print_r(gettimeofday());
+echo "</pre>";
+
+echo gettimeofday(true); // This Parameter is use for Float value And skip the array
+
+
+//// localtime() Function is use for Have Local Time of our local system in the index Array Form.
+
+echo $time['usec'];
+
+echo "<pre>";
+print_r(localtime(time(),true)); // This two parameters For Associatine Array
+echo "</pre>";
+
+// For Old date
+
+$time = mktime(3,12,0,6,4,22);
+
+echo "<pre>";
+print_r(localtime($time,true)); // This two parameters For Associatine Array
+echo "</pre>";
+*/
+ 
+
+                ####### date_parse() Function #########
+
+/*            
+//// date_parse() Function is use for Show Array for date and time of Given that in the Function.
+
+echo "<pre>";
+print_r(date_parse('2001-04-06 2:12:23.6'));
+echo "</pre>";
+
+// Specific date/time with array key
+
+$date = date_parse('2001-04-06 2:12:23.6');
+
+echo $date['year'];
+
+//// date_parse_from_format() Function is use for have Time Array With Given Formate.
+
+echo "<pre>";
+print_r(date_parse_from_format('d.m.y','12.04.2022'));
+echo "</pre>";
+*/
+
+
+            ####### strtotime() , strftime() $ Gmstrftime Function #########
+
+/*
+//// strtotime() function is use for Convert String into time.
+
+echo date("d-F-Y",strtotime('now')) . "<br>";
+echo date("d-F-Y",strtotime('23 April 2006')) . "<br>";
+echo date("d-F-Y",strtotime('+1 month')) . "<br>";
+echo date("h:i:s A",strtotime('+3 hours')) . "<br>";
+echo date("d-F-Y",strtotime('+3 week')) . "<br>";
+echo date("d-F-Y",strtotime('Last day of Last Month')) . "<br>";
+echo date("d-F-Y h:i:s A",strtotime('+1 week 3 Hours 5 minutes 0 second')) . "<br>";
+echo date("d-F-Y",strtotime('previous Friday')) . "<br>";
+
+
+//// strftime() Function is use for Convert Time into String. In this Function we use some Spacial string start with % called literal.
+
+echo strftime("%B %d %Y, %X %Z") . "<br>";
+
+// For Past date with mktime Function 
+
+echo strftime("%B %d %Y, %X %Z",mktime(22,04,00,4,6,2001)) . "<br>";
+
+echo strftime("%d/%m/%Y, %H:%M:%S",mktime(22,04,00,4,6,2001)) . "<br>";
+
+
+//// gmstrftime() Function is use for show gmt or utc time zone.
+
+echo gmstrftime("%d/%m/%Y, %H:%M:%S",mktime(22,04,00,4,6,2001)) . "<br>";
+*/
+
+
+
+            ####### date_time_set() Function #########
+
+/*
+// date_time_set(objest,hours,minutes,seconds,microsecond) Function is use for add time/date into the existence time/date.
+
+$date = date_create("2015-05-15"); // dat_create() use for Craete Any date
+
+date_time_set($date , 02,35);
+
+echo date_format($date , "d/M/Y") . "<br>";  // date_formate() Use for show date with formate and object
+
+date_time_set($date , 02,35,45);
+
+echo date_format($date , "d/M/Y  H:m:s A");
+*/
+
+
+
+            ####### TimeZone Function #########
+
+/*            
+// There are Six type of timeZone Function are as follous.            
+
+
+//// 1.date_default_timezone_get() Function Find bydefault Time Zone Set on our Local System.
+
+echo date_default_timezone_get() . "<br>";
+
+//// 2.date_default_timezone_set() Function is use for set time Zone.
+
+date_default_timezone_set('Asia/Karachi');
+echo date_default_timezone_get() . "<br>";
+
+// An other Way for set and get TimeZone
+
+$tzm = timezone_open('Asia/Karachi'); // This Function For Set Time Zone
+
+echo timezone_name_get($tzm) . "<br>"; // This Function For Show Time Zone
+
+//// 3.timezone_location_get() Function is use for Find location of our Current TimeZone.This Function Return an Array with latitude and longitude.
+
+echo "<pre>";
+print_r(timezone_location_get($tzm));
+echo "</pre>";
+
+//// 4.timezone_identifiers_list() Function is use for have list Of All Time Zone of the World.
+
+echo "<pre>";
+print_r(timezone_identifiers_list());
+echo "</pre>";
+
+echo "<pre>";
+print_r(timezone_identifiers_list(1)); // Here 1 is for african time Zone
+echo "</pre>";
+
+echo "<pre>";
+print_r(timezone_identifiers_list(1024)); // Here 2 is for American time Zone
+echo "</pre>";
+
+// Note: Spacific Code For All Countres in the Folder of Relative Images
+*/
+
+
+
+
 
 
 
