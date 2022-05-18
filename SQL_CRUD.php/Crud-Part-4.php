3abd159e132c4170f13873d60160ebf77d485175
Muhammad Talha <muhammadtalha3801@gmail.com>
2022-05-18 17:30:36 +0500

    new Code;

---
+++ SQL_CRUD.php/Crud-Part-4.php
@@ -0,0 +1,6 @@
+
+<!-- PHP MySQL CRUD - Delete Data Lac # 147 -->
+
+Code related of this in the delete.php file of the crud Folder.
+
+In this Tutorial we learn about Delete data from the data table and this is the Last Part of CRUD.
--- SQL_CRUD.php/Crud_Part-3.php
+++ SQL_CRUD.php/Crud_Part-3.php
@@ -1,6 +1,30 @@
 
-<!-- PHP MySQL CRUD - Update Data Lac # 145 -->
+<!-- PHP MySQL CRUD - Update Data Tutorial-I Lac # 145 -->
 
 Practise for Update we Code on Update.php file in html_Crud Folder
 
+
+
 For Work on this first we send Id with the Edit Button. 
+
+Then with he id name we fill the data of selected used in the Update Page 
+
+then Using Update Query in the Saprate file file name is updatedata.php then work for update on this file
+the next all Code in this file.
+
+
+<!-- PHP MySQL CRUD - Update Data Tutorial-II Lac # 146 -->
+
+In this Update Tutorial we use update.php file into the crud Folder.First Step we Write $_SERVER['PHP_SELF'] in the ection of this form then we code for the update with the id.
+
+First we Code for Update press Buttun if this Buttun Press then All Code Run. By if(isset($_POST['showbtn'])){
+    connection
+    Query
+    run query 
+    
+}
+
+then we code for user ID then user Put an Id we Fetch All Data of this Id and fill thid data into the update input Feilds then we press update of change Some data into the form then we Udate it.
+
+
+in this cse we use all code from the updatedate.php file.
